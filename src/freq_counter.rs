8459fc0d273a606b543749367656859860309cb8
Paul Fornage <paul@fornage.net>
2024-10-11 19:36:54 -0700

    added rolling average to freq counter

--- src/freq_counter.rs
+++ src/freq_counter.rs
@@ -13,6 +13,8 @@ const PULSE_MEASURE_WINDOW_US: u64 = 100_000;
 
 const MIN_DELAY_BETWEEN_UPDATES: embassy_time::Duration = embassy_time::Duration::from_micros(PULSE_MEASURE_WINDOW_US);
 
+const RPM_HISTORY_LEN: usize = 3;
+
 
 #[embassy_executor::task]
 pub async fn freq_counter_task(r: FreakyResources) {
@@ -20,6 +22,7 @@ pub async fn freq_counter_task(r: FreakyResources) {
     let pwm = pwm::Pwm::new_input(r.freak_slice, r.freak_pin, Pull::None, InputMode::RisingEdge, cfg);
     let mut start_time: embassy_time::Instant;
     let mut update_ticker = embassy_time::Ticker::every(MIN_DELAY_BETWEEN_UPDATES);
+    let mut rpm_history: circular_buffer::CircularBuffer<RPM_HISTORY_LEN, f64> = circular_buffer::CircularBuffer::<RPM_HISTORY_LEN, f64>::new();
     let mut pulses: u16;
     loop {
         start_time = embassy_time::Instant::now();
@@ -29,8 +32,8 @@ pub async fn freq_counter_task(r: FreakyResources) {
         pulses = pwm.counter();
         
         let elapsed_time_s = start_time.elapsed().as_ticks() as f64 / embassy_time::TICK_HZ as f64;
-        
-        send_rpm((pulses as f64 * 3.0 * 60.0)/(elapsed_time_s * RPM_PULSES_PER_REV)).await;
+        rpm_history.push_back((pulses as f64 * 3.0 * 60.0)/(elapsed_time_s * RPM_PULSES_PER_REV));
+        send_rpm(rpm_history.iter().sum::<f64>() / RPM_HISTORY_LEN as f64).await;
     }
 }
 
